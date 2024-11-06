39a060d991700ecf07cc7150888a1d58a266434f
nilver987 <nilver.1967@gmail.com>
2024-11-05 20:57:27 -0500

    10

--- ms-travel/src/main/java/com/example/mstravel/repository/TravelRepository.java
+++ ms-travel/src/main/java/com/example/mstravel/repository/TravelRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 public interface TravelRepository extends JpaRepository<Travel, Integer> {
-    List<Travel> findById(String id);
+
 }
