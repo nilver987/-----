044a727b5df39a8e510f263b0d7ed6f94be8e237
nilver987 <nilver.1967@gmail.com>
2024-11-05 20:42:36 -0500

    7

--- ms-travel/src/main/java/com/example/mstravel/entity/Travel.java
+++ ms-travel/src/main/java/com/example/mstravel/entity/Travel.java
@@ -1,4 +1,27 @@
 package com.example.mstravel.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Travel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String plate;
+    private String model;
+    private String mark;
+    private Integer year;
+    private Integer capacity;
+    private String status;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "maintenance_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Maintenance maintenance;
+}
+
+
 }
--- ms-travel/src/main/java/com/example/mstravel/repository/TravelRepository.java
+++ ms-travel/src/main/java/com/example/mstravel/repository/TravelRepository.java
@@ -1,4 +1,9 @@
 package com.example.mstravel.repository;
 
-public interface TravelRepository {
+import com.example.msvehicle.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+public interface TravelRepository extends JpaRepository<Vehicle, Integer> {
+    List<Vehicle> findByPlate(String plate);
 }
