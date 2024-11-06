b2e1638fbadc03f6f35392d5a59ccab03eb24e3c
nilver987 <nilver.1967@gmail.com>
2024-11-05 19:16:43 -0500

    first commit

--- ms-vehicle/src/main/java/com/example/msvehicle/repository/MaintenanceRepository.java
+++ ms-vehicle/src/main/java/com/example/msvehicle/repository/MaintenanceRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MaintenanceRepository extends JpaRepository<Maintenance, Integer> {
-    List<Maintenance> findByCode(String code);
+    List<Maintenance> findByDescription(String description);
 }
