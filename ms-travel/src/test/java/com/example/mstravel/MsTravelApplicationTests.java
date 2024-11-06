448c3fa417359f7086e90ed599bbadff913bfcd4
nilver987 <nilver.1967@gmail.com>
2024-11-05 20:33:40 -0500

    lombok

---
+++ ms-travel/src/test/java/com/example/mstravel/MsTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mstravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
