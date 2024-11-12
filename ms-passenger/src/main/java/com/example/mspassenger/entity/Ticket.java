fe290a015b1e79817eaa4a90fe763ff36ba7d346
nilver987 <nilver.1967@gmail.com>
2024-11-12 17:40:10 -0500

    entity

--- ms-passenger/src/main/java/com/example/mspassenger/entity/Ticket.java
+++ ms-passenger/src/main/java/com/example/mspassenger/entity/Ticket.java
@@ -1,8 +1,11 @@
 package com.example.mspassenger.entity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Date;
 
 import lombok.Data;
 
@@ -12,8 +15,24 @@ public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String description;
-    private LocalDate maintenanceDate;
-    private String status;  // completed, pending
-    private Double cost;
+    private String seatNumber;
+    private BigDecimal price;
+    private Date purchaseDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservations_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Reservations reservations;
+
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    public enum Status {
+        PAID,
+        PENDING,
+        CANCELLED
+    }
 }
