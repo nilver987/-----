643bf8a702841fe00152dd04bfc4c34c822fd532
nilver987 <nilver.1967@gmail.com>
2024-11-11 21:40:36 -0500

    terminal

---
+++ ms-conductor/src/test/java/com/example/msconductor/MsConductorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msconductor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConductorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
