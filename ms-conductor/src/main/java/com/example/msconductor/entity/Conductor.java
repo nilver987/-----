830df04012a6432b9a0613d3e226bbf4bca8c735
nilver987 <nilver.1967@gmail.com>
2024-11-12 17:30:35 -0500

    entity

--- ms-conductor/src/main/java/com/example/msconductor/entity/Conductor.java
+++ ms-conductor/src/main/java/com/example/msconductor/entity/Conductor.java
@@ -1,8 +1,5 @@
 package com.example.msconductor.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -12,4 +9,16 @@ public class Conductor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    public enum Role {
+        conductor,
+        asistente
+    }
+
+
+
+
+    //FOREIGN KEY (trip_id) REFERENCES trips(id),
+    // FOREIGN KEY (driver_id) REFERENCES users(id)
 }
